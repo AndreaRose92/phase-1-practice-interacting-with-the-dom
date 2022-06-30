14c496fa128b0dbe8249929d0dba1e69d8dc6bf4
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-06-30 11:19:10 -0400

    plus and minus

--- js/challenge.js
+++ js/challenge.js
@@ -1,16 +1,27 @@
-let counter = parseInt(document.getElementById("counter").textContent)
-const input =  document.querySelectorAll('button')
+// let counter = parseInt(document.getElementById("counter").textContent)
+const minus =  document.getElementById('minus')
+const plus = document.getElementById('plus')
 
-function counterPlus() {
-    counter++
-    return counter
-}
 
-document.addEventListener('click', input) {
-    if (input = "plus") {
-        counterPlus()
-    }
-}
+// function counterPlus() {
+//     let a = document.getElementById('counter')
+//     let b = parseInt(a.innerText)
+//     a.innerText = b+1
+// }
+
+plus.addEventListener('click', function() {
+    var a = document.getElementById('counter')
+    b = parseInt(a.innerText)
+    a.innerText = b+1
+})
+
+minus.addEventListener('click', function() {
+    var a = document.getElementById('counter')
+    b = parseInt(a.innerText)
+    a.innerText = b-1
+})
+
+
 // setInterval(increment(), 500)
 
 // function increment() {
